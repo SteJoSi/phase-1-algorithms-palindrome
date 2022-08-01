ae79e53c6049a3bd9e47f1287e3ccccdf9255bd5
Stephanie Sison <sjsison27@icloud.com>
2022-08-01 13:16:50 -0700

    commit tree b8e152fd5b3cc3065f2c2e5c9db69659afda524a

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordValues = word.split('');
+  // console.log('split', wordValues)
+  const wordReverse = wordValues.reverse();
+  // console.log('reverse', wordReverse)
+  const wordJoin = wordReverse.join('')
+  // console.log('join', wordJoin)
+
+
+  if(word === wordJoin){
+      return true
+  } else {return false}
 }
 
 /* 
   Add your pseudocode here
+- take word and split each one to be its on array
+- use the reverse() to take each word and check if the letters match being read backwards
+- join the letter back together
 */
-
-/*
-  Add written explanation of your solution here
+ 
+ /*
+ Add written explanation of your solution here
+ The function will take in a word, evaluate it and if it sees it as a Palindrome, it will return 'true', otherwise it will return 'false'
 */
 
 // You can run `node index.js` to view these console logs
